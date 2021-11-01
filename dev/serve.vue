8cc1e2a0bfc010fcc4ff0980c4fb7c7e1e75e4b3
Seantsang <9394wing@gmail.com>
2021-11-01 10:04:41 +0800

    init

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { WclUiSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  WclUiSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <wcl-ui-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as WclUiSample } from './wcl-ui-sample.vue';
